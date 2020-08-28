a4c00d6dbc3612196c3b04db147dc10960905e43
Lucas Caetano <lucas.caetanov2013@gmail.com>
2020-08-28 15:21:01 -0300

    Inicio

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Ashion Template">
+    <meta name="keywords" content="Ashion, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ashion | Template</title>
+
+    <?php 
+include_once 'header.html'
+    ?>
+<style type="text/css">
+    .product__item__pic{
+       border-radius: 10px;
+    }
+    .pic{
+       border-radius: 10px; 
+    }
+
+</style>
+   
+<!-- Banner Section Begin -->
+<section class="banner set-bg" data-setbg="img/banner/bannerteste.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-7 col-lg-8 m-auto">
+                <div class="banner__slider owl-carousel">
+                    <div class="banner__item">
+                        <div class="">
+                            <span>Mutual</span>
+                            <h1>Drop 2</h1>
+                            <a href="#">Comprar agora</a>
+                        </div>
+                    </div>
+                    <div class="banner__item">
+                        <div class="">
+                            <span>Mutual</span>
+                            <h1>Drop 2</h1>
+                            <a href="#">Comprar agora</a>
+                        </div>
+                    </div>
+                    <div class="banner__item">
+                        <div class="">
+                            <span>Mutual</span>
+                            <h1>Drop 2</h1>
+                            <a href="#">Comprar agora</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Banner Section End -->
+<!-- Product Section Begin -->
+
+<center>
+    <hr>
+<h2>Produtos:</h2><br>
+                <div class="col-lg-9 col-md-9">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="img/shop/ninja.png">
+                                    <div class="label new">New</div>
+                                    <ul class="product__hover">
+                                        <li><a href="img/shop/ninja.png" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                        <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                        <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                    </ul>
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="#">"Ninja"</a></h6>
+                                  
+                                    <div class="product__price">R$ 60.0</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="img/shop/boi.jpg">
+                                    <ul class="product__hover">
+                                        <li><a href="img/shop/boi.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                        <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                        <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                    </ul>
+
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="#">"Boicore"</a></h6>
+                                 
+                                    <div class="product__price">R$ 60.0</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="img/shop/caveira.jpg">
+                                    <ul class="product__hover">
+                                        <li><a href="img/shop/caveira.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                        <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                        <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                    </ul>
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="#">"Sem titulo"</a></h6>
+                                  
+                                    <div class="product__price">R$ 60.0</div>
+                                </div>
+                               </div>
+                           </div>
+                       </div>
+
+   
+
+<hr>
+
+<!-- Discount Section Begin -->
+<section class="discount">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-4">
+                <div class="discount__pic">
+                    <img class="pic" src="img/shop/teste.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-4">
+                
+                         <div class="discount__pic">
+                    <img class="pic" src="img/shop/teste2.jpg" alt="">
+                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Discount Section End -->
+
+<?php 
+include_once 'footer.html';
+?>
---
+++ shop.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Ashion Template">
+    <meta name="keywords" content="Ashion, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ashion | Template</title>
+
+<?php include_once 'header.html'?>
+    <!-- Breadcrumb Begin -->
+    <style type="text/css">
+    .product__item__pic{
+       border-radius: 10px;
+    }
+    .pic{
+       border-radius: 10px; 
+    }
+
+</style>
+    <div class="breadcrumb-option">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb__links">
+                        <a href="./index.php"><i class="fa fa-home"></i> Home</a>
+                        <span>Shop</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+<center>
+    <hr>
+<h2>Produtos:</h2><br>
+                <div class="col-lg-9 col-md-9">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="img/shop/ninja.png">
+                                    <div class="label new">New</div>
+                                    <ul class="product__hover">
+                                        <li><a href="img/shop/ninja.png" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                        <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                        <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                    </ul>
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="#">"Ninja"</a></h6>
+                                  
+                                    <div class="product__price">R$ 60.0</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="img/shop/boi.jpg">
+                                    <ul class="product__hover">
+                                        <li><a href="img/shop/boi.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                        <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                        <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                    </ul>
+
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="#">"Boicore"</a></h6>
+                                 
+                                    <div class="product__price">R$ 60.0</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="img/shop/caveira.jpg">
+                                    <ul class="product__hover">
+                                        <li><a href="img/shop/caveira.jpg" class="image-popup"><span class="arrow_expand"></span></a></li>
+                                        <li><a href="#"><span class="icon_heart_alt"></span></a></li>
+                                        <li><a href="#"><span class="icon_bag_alt"></span></a></li>
+                                    </ul>
+                                </div>
+                                <div class="product__item__text">
+                                    <h6><a href="#">"Sem titulo"</a></h6>
+                                  
+                                    <div class="product__price">R$ 60.0</div>
+                                </div>
+                               </div>
+                           </div>
+                       </div>
+
+   
+
+   
+<?php 
+include_once 'footer.html';
+?>
